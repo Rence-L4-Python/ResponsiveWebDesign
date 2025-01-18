32421be0e7583d608fda32c50c354daced2d3009
Rence-L4-Python <145338693+Rence-L4-Python@users.noreply.github.com>
2025-01-18 18:44:48 +0400

    initial-update

---
+++ Assessment 2/script.js
@@ -0,0 +1,26 @@
+function updateParagraph(){
+    let paragraph = document.getElementById("change-p");
+    let media_768 = window.matchMedia("(min-width: 768px)");
+    let media_1200 = window.matchMedia("(min-width: 1200px)");
+
+    if (media_1200.matches){
+        paragraph.innerHTML = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam finibus erat in diam tincidunt vehicula. 
+        Morbi accumsan dolor ipsum, vitae accumsan sapien condimentum consectetur.
+        Etiam aliquam leo dolor, eget interdum justo feugiat id. Donec blandit ante at posuere 
+        convallis. Morbi blandit arcu at facilisis maximus. Sed accumsan suscipit venenatis. 
+        Vivamus pellentesque, odio sed molestie placerat, erat nibh scelerisque mi, sit amet 
+        tincidunt purus lorem id mi. Sed id dolor cursus, dictum mi vel, porttitor est.`;
+    } 
+    else if (media_768.matches){
+        paragraph.innerHTML = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam finibus erat in diam tincidunt vehicula. 
+        Morbi accumsan dolor ipsum, vitae accumsan sapien condimentum consectetur. 
+        Etiam aliquam leo dolor, eget interdum justo feugiat id.`;
+    } 
+    else{
+        paragraph.innerHTML = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam finibus erat in diam tincidunt vehicula.`;
+    }
+}
+
+updateParagraph();
+
+window.addEventListener("resize", updateParagraph);
